d526e95e427dd98e841b1eb73bf01ca4c1c84006
Adam Legg <adam.legg@gmail.com>
2020-05-08 15:16:08 -0600

    Do not resolve values in oath

--- lib/ueberauth/strategy/google/oauth.ex
+++ lib/ueberauth/strategy/google/oauth.ex
@@ -26,7 +26,7 @@ defmodule Ueberauth.Strategy.Google.OAuth do
   """
   def client(opts \\ []) do
     config = Application.get_env(:ueberauth, __MODULE__, [])
-    opts = @defaults |> Keyword.merge(opts) |> Keyword.merge(config) |> resolve_values()
+    opts = @defaults |> Keyword.merge(opts) |> Keyword.merge(config)
     json_library = Ueberauth.json_library()
 
     OAuth2.Client.new(opts)
@@ -73,13 +73,4 @@ defmodule Ueberauth.Strategy.Google.OAuth do
     |> put_header("Accept", "application/json")
     |> OAuth2.Strategy.AuthCode.get_token(params, headers)
   end
-
-  defp resolve_values(list) do
-    for {key, value} <- list do
-      {key, resolve_value(value)}
-    end
-  end
-
-  defp resolve_value({m, f, a}) when is_atom(m) and is_atom(f), do: apply(m, f, a)
-  defp resolve_value(v), do: v
 end
